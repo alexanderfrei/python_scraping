851a0779156dbfe683ea05d73bae7a578dcce997
alexanderfrei <whutmail@gmail.com>
2017-01-16 22:54:38 +0300

    Check API examples

--- tools/api_examples/freegeoip.py
+++ tools/api_examples/freegeoip.py
@@ -4,6 +4,7 @@ from urllib.request import urlopen
 def getCountry(ipAddress):
     response = urlopen("http://freegeoip.net/json/"+ipAddress).read().decode('utf-8')
     responseJson = json.loads(response)
+    print(responseJson)
     return responseJson.get("country_code")
 
 print(getCountry("50.78.253.58"))
--- tools/formats and db/jsonParsing.py
+++ tools/formats and db/jsonParsing.py
@@ -1,6 +1,8 @@
 import json
 
-jsonString = '{"arrayOfNums":[{"number":0},{"number":1},{"number":2}],"arrayOfFruits":[{"fruit":"apple"},{"fruit":"banana"},{"fruit":"pear"}]}'
+jsonString = '''{"arrayOfNums":[{"number":0},{"number":1},{"number":2}],
+"arrayOfFruits":[{"fruit":"apple"},{"fruit":"banana"},{"fruit":"pear"}]}'''
+
 jsonObj = json.loads(jsonString)
 
 print(jsonObj.get("arrayOfNums"))
